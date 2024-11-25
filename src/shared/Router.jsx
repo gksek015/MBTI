df5e8d1e5041c080ba94fa9e0fe91d68a919fcb4
gksekdud <dayoung1490@gmail.com>
2024-11-25 23:53:26 +0900

    fix: Router.jsx

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -3,11 +3,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
 import Login from "../pages/Login"
 import Signup from "../pages/Signup"
-import TestPage from "../pages/TestPage"
 import TestResult from "../pages/TestResult"
-import TestResultList from "../pages/TestResultList"
 import Profile from "../pages/Profile"
 import ProtectedRoute from "../components/ProtectedRoute"
+import Test from "../pages/Test"
 
 
 const Router = () => {
@@ -17,9 +16,8 @@ const Router = () => {
         <Route element={<ProtectedRoute/>}>
             <Route path="/" element={<Home/>}/>
             <Route path="/profile" element={<Profile/>}/>
-            <Route path="/test-page" element={<TestPage/>}/>
-            <Route path="/test-result" element={<TestResult/>}/>
-            <Route path="/test-list" element={<TestResultList/>}/>
+            <Route path="/test" element={<Test/>}/>
+            <Route path="/result" element={<TestResult/>}/>
         </Route>
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<Signup/>}/>
